3cf2bb93c6c5ca5599674ab6e103810e5b81a824
Isaiah-code2035 <oluwabamiseolanipekun@gmail.com>
2022-01-05 20:37:52 +0100

    changes

--- sessions/app.js
+++ sessions/app.js
@@ -2,28 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const Router = require('./Routes')
+
 
 
 const app = express();
 //coennect to MongoDb
 
-mongoose.connect('mongodb+srv://login:<Asiwaju@2035>@cluster0.z5p8s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    }
-
-);
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function() {
-    console.log("Connected successfully")
-});
-
-//use Middleware
-app.use(Router)
 app.use(morgan('combined'));
 app.use(session({
 
